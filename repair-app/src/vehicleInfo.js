3e70fdcd3db8d9a14489761c6a538e61ab4a13fb
Rosie Jones <rosiegracejones@gmail.com>
2023-11-07 14:01:09 +0000

    initial plan & dummy data added

---
+++ repair-app/src/vehicleInfo.js
@@ -0,0 +1,7 @@
+let vehicleInfo = ({make: "Toyota", model: "Camry", registrationNumber: "ABC123", notes: "Needs new tires", repairStatus: "In Progress"}, 
+{make: "Renault", model: "Clio", registrationNumber: "DEF456", notes: "Needs new brakes", repairStatus: "In Progress"},
+{make: "Ford", model: "Fiesta", registrationNumber: "GHI789", notes: "Needs new engine", repairStatus: "In Progress"},
+{make: "Volkswagen", model: "Golf", registrationNumber: "JKL012", notes: "Needs new tires", repairStatus: "In Progress"}
+)
+
+export default vehicleInfo;
